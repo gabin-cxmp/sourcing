63f193012ea4ec956c6870534a7bf334ec6dcbc9
gabin-cxmp <gabin.touitou@comexposium.com>
2025-08-26 14:14:11 +0200

    Update constants.js

--- sourcing/assets/js/constants.js
+++ sourcing/assets/js/constants.js
@@ -2,9 +2,13 @@
 export const CONFIG = {
   itemsPerPage: 12,
   csvUrls: [
-    'https://docs.google.com/spreadsheets/d/e/2PACX-1vTda139g0YsEDh0AW3PQ2hGvZpFlSQlS4QlOqjUKN5tJWCzgXmRDl-S8k3V3drnHyD3ax-_zqnyAoIp/pub?gid=0&single=true&output=csv',
-    'https://docs.google.com/spreadsheets/d/e/2PACX-1vQFMnAIVzcRLBXNXvNPcoDe3fmWeRXteipd6O5qvwOADkRcDc4VuF7dWuqP5s7HEFOif0eykCQM4hYm/pub?gid=0&single=true&output=csv',
-    'https://docs.google.com/spreadsheets/d/e/2PACX-1vTIjq-iOQA2VUcdOIWXL1VhZXZ_OWnwSbhApfoN1U8mTMSeIBLiFO8TzEIKLLFjeHrgVqpbQDxJuKrx/pub?gid=0&single=true&output=csv'
+  //  'https://docs.google.com/spreadsheets/d/e/2PACX-1vTda139g0YsEDh0AW3PQ2hGvZpFlSQlS4QlOqjUKN5tJWCzgXmRDl-S8k3V3drnHyD3ax-_zqnyAoIp/pub?gid=0&single=true&output=csv',
+  //  'https://docs.google.com/spreadsheets/d/e/2PACX-1vQFMnAIVzcRLBXNXvNPcoDe3fmWeRXteipd6O5qvwOADkRcDc4VuF7dWuqP5s7HEFOif0eykCQM4hYm/pub?gid=0&single=true&output=csv',
+  //  'https://docs.google.com/spreadsheets/d/e/2PACX-1vTIjq-iOQA2VUcdOIWXL1VhZXZ_OWnwSbhApfoN1U8mTMSeIBLiFO8TzEIKLLFjeHrgVqpbQDxJuKrx/pub?gid=0&single=true&output=csv'
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vTda139g0YsEDh0AW3PQ2hGvZpFlSQlS4QlOqjUKN5tJWCzgXmRDl-S8k3V3drnHyD3ax-_zqnyAoIp/pub?output=csv',
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vQFMnAIVzcRLBXNXvNPcoDe3fmWeRXteipd6O5qvwOADkRcDc4VuF7dWuqP5s7HEFOif0eykCQM4hYm/pub?output=csv',
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vTIjq-iOQA2VUcdOIWXL1VhZXZ_OWnwSbhApfoN1U8mTMSeIBLiFO8TzEIKLLFjeHrgVqpbQDxJuKrx/pub?output=csv'
+
   ],
   specialFilters: ['handmade', 'recycled', 'organic', 'ethical-manufacturing', 'limited-edition', 'white-label']
 };
